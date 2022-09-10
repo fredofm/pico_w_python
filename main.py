7f4da4f4462770b66ecaf896fc2e49137716e03d
Alfredo Fernandez Manzano <afernandezma@dxc.com>
2022-09-10 16:58:59 +0200

    Adjust server interaction

--- main.py
+++ main.py
@@ -1,22 +1,132 @@
 import wifi
 import ujson
 import urequests
+import machine
+import _thread
+import utime
+import sys
+import time
+import gc
+
+LED = machine.Pin("LED", machine.Pin.OUT)
+RELAY = machine.Pin(15, machine.Pin.OUT)
+
+def init():
+    gc.enable()
+    LED.off()
+    RELAY.value(0)
 
 def readSecrets():
+    print("reading Wifi secrets")    
+    
     with open('secrets.json') as fp:
         secrets = ujson.loads(fp.read())
         return secrets
 
-secrets = readSecrets()
 
-wifi.printStatusCodes()
-wlan = wifi.connect(secrets['wifi']['ssid'], secrets['wifi']['pass'])
+def wifiConnect():
+    print("Tryin to create Wifi connection")    
+        
+    if not wifi.wlan.isconnected():                    
+        # wifi.printStatusCodes()
+        
+        try:            
+            secrets = readSecrets()
+            wifi.connect(secrets['wifi']['ssid'], secrets['wifi']['pass'])            
+        except Exception as e:            
+            print("Exception when trying to connect " + secrets['wifi']['ssid'])
+            print(repr(e))
+            sys.print_exception(e)
 
-if wlan.isconnected():
-    status = wlan.ifconfig()
-    print('IP = ' + status[0])
+        if wifi.wlan.isconnected():
+            LED.on()
+            status = wifi.wlan.ifconfig()
+            print('wifi_management IP = ' + status[0])          
+        else:
+            LED.off()
+    else:        
+        LED.on()
+        
 
-    # Make GET request
-    r = urequests.get("http://date.jsontest.com")
-    print(r.json())
-    r.close()
+def activateProp():
+    print("Activating Prop")
+    RELAY.value(1)    
+    time.sleep(60)
+    deactivateProp()
+    
+def deactivateProp():
+    print("De-activating Prop")    
+    RELAY.value(0)        
+
+def showLEDError():
+    print("Show error")
+    
+    LED.toggle()
+    time.sleep(0.15)
+    LED.toggle()
+    time.sleep(0.15)
+    LED.toggle()
+    time.sleep(0.15)
+    LED.toggle()
+    time.sleep(0.15)
+    LED.toggle()
+    time.sleep(0.15)
+    LED.toggle()
+    time.sleep(0.15)
+    LED.toggle()
+    time.sleep(0.15)
+    LED.toggle()
+
+def getPropRemoteStatus():
+    activatePropStatus = False
+    propId = 2
+    urlBase = "http://192.168.123.60:8080/api/v1/props/"
+    url = urlBase + str(propId)
+        
+    try:
+        # Make GET request
+        print("Making GET request: {0}".format(url))
+        res = urequests.get(url)
+        print(res.json()['running'])
+        activatePropStatus = res.json()['running']
+    except Exception as e:
+        print("Exception occurs when GET " + url)
+        print(repr(e))
+        sys.print_exception(e)
+        showLEDError()
+    finally:
+        try:
+            res.close()
+        except:
+            print("Skipping response closing")
+
+    gc.collect()  
+    
+    return activatePropStatus
+
+init()
+wifiConnect()
+
+#_thread.start_new_thread(threadExecution, ())
+
+last_time1 = time.time()  # holds when we did something #1
+remotePropStatus = False
+
+while True:
+    
+    if not wifi.wlan.isconnected():
+        wifiConnect()
+    
+    if (time.time() - last_time1 > 20):
+        remotePropStatus = getPropRemoteStatus()    
+    
+    print("remotePropStatus: " + str(remotePropStatus) + " Wifi isConnected():" + str(wifi.wlan.isconnected()))
+    
+    if remotePropStatus or (time.time() - last_time1 > 120.0):  # every 2 seconds        
+        remotePropStatus = False
+        activateProp()
+        last_time1 = time.time() # save when we do the thing
+        
+        
+    utime.sleep(0.5) 
+ 
--- wifi.py
+++ wifi.py
@@ -1,5 +1,7 @@
 import time
+import machine
 import network
+#import rp2
 
 STATUSCODES = {network.STAT_IDLE: 'no connection and no activity',
                network.STAT_CONNECTING: 'connecting in progress',
@@ -8,6 +10,13 @@ STATUSCODES = {network.STAT_IDLE: 'no connection and no activity',
                network.STAT_CONNECT_FAIL: 'failed due to other problems',
                network.STAT_GOT_IP: 'connection successful'}
 
+#rp2.country('ES')
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+# power mode on (deactivate powersaving mode)
+wlan.config(pm = 0xa11140)
+
 def printStatusCodes():
     for code in STATUSCODES.keys():
         printStatusCode(code)
@@ -15,26 +24,21 @@ def printStatusCodes():
 def printStatusCode(code):
     print('#[{:>2}] {}'.format(code, STATUSCODES[code]))
 
-def connect(ssid, password):
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
+def connect(ssid, password):    
+    if not wlan.isconnected():
+        print('Not Wifi connection available. Trying to connect ' + ssid)
+        wlan.ifconfig(('192.168.123.116', '255.255.255.0', '192.168.123.76', '192.168.123.76'))
+        wlan.connect(ssid, password)
 
-    if wlan.isconnected():
-        print('Last connection is still active, disconnecting...')
-        wlan.disconnect()
+        # Wait for connect or fail
+        max_wait = 10        
+        while max_wait > 0:
+            if wlan.status() == network.STAT_GOT_IP:
+                break
+            max_wait -= 1
+            time.sleep(1)
 
-    wlan.connect(ssid, password)
-
-    # Wait for connect or fail
-    max_wait = 10
-    print('Trying connect ' + ssid + '...')
-    while max_wait > 0:
-        if wlan.status() == network.STAT_GOT_IP:
-            break
-        max_wait -= 1
-        time.sleep(1)
-
-    printStatusCode(wlan.status())
-
-    return wlan
+        printStatusCode(wlan.status())
+    else:
+        print('Last connection is still available') 
 
